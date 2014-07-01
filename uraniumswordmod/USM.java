1f2b78c93ccc4c56ecca3742e0d83c25c4944afa
Denvys5 <denvys5@gmail.com>
2014-07-01 22:29:16 +0300

    OR3: Config File

--- uraniumswordmod/USM.java
+++ uraniumswordmod/USM.java
@@ -1,5 +1,7 @@
 package assets.uraniumswordmod;
 
+import java.io.File;
+
 import assets.uraniumswordmod.block.BlockNetherStar;
 import assets.uraniumswordmod.block.BlockUranium;
 import assets.uraniumswordmod.block.FurnaceUranium;
@@ -19,6 +21,7 @@ import net.minecraft.item.EnumToolMaterial;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.potion.Potion;
+import net.minecraftforge.common.Configuration;
 import net.minecraftforge.common.EnumHelper;
 import net.minecraftforge.oredict.OreDictionary;
 import net.minecraftforge.oredict.ShapedOreRecipe;
@@ -64,12 +67,36 @@ public class USM {
 	public static CreativeTabs USMTab;
 	
 	
-//	public static int oreuraniumblockID;
+	public static int oreuraniumblockID;
+	public static int blockuraniumblockID;
+	public static int blocknetherstarblockID;
+	public static int sworduraniumitemID;
+	public static int ingotinfuseduraniumitemID;
+	public static int ingoturaniumitemID;
+	public static int stickironitemID;
+	public static int furnaceuraniumidleblockID;
+	public static int furnaceuraniumactiveblockID;
+	
+	public void initConfiguration(FMLInitializationEvent event){
+		Configuration config = new Configuration(new File("config/USM/UraniumSwordMod.cfg"));
+		config.load();
+		oreuraniumblockID = config.get("Blocks", "Uranium Ore", 500).getInt();
+		blockuraniumblockID = config.get("Blocks", "Uranium Block", 502).getInt();
+		blocknetherstarblockID = config.get("Blocks", "Nether Star Block", 501).getInt();
+		sworduraniumitemID = config.get("Items", "Uranium Sword", 1551).getInt();
+		ingotinfuseduraniumitemID = config.get("Items", "Infused Uranium Ingot", 1553).getInt();
+		ingoturaniumitemID = config.get("Items", "Uranium Ingot", 1552).getInt();
+		stickironitemID = config.get("Items", "Iron Stick", 1550).getInt();
+		furnaceuraniumidleblockID = config.get("Tile Entities", "Uranium Furnace", 503).getInt();
+		furnaceuraniumactiveblockID = config.get("Tile Entities", "Uranium Furnace Active", 504).getInt();
+		config.save();
+	}
 
 	
 	@EventHandler
 	public void Load(FMLInitializationEvent event)
 	{
+		this.initConfiguration(event);
 		GameRegistry.registerWorldGenerator(new OreGenerator());
 		
 		USMTab = new CreativeTabs("uraniumswordmodtab"){
@@ -81,23 +108,28 @@ public class USM {
 		
 		proxy.registerRandomStuff();
 		
-		oreuranium = new OreUranium(500).setUnlocalizedName("Uranium Ore");
+		//oreuranium = new OreUranium(500).setUnlocalizedName("Uranium Ore");
+		oreuranium = new OreUranium(this.oreuraniumblockID).setUnlocalizedName("Uranium Ore");
 		GameRegistry.registerBlock(oreuranium, "oreuranium");
 		LanguageRegistry.instance().addNameForObject(oreuranium, "en_US", "Uranium Ore");
         LanguageRegistry.instance().addNameForObject(oreuranium, "ru_RU", "Фу, не знать английский");
         
-        blocknetherstar = new BlockNetherStar(501).setUnlocalizedName("Nether Star Block");
+        //blocknetherstar = new BlockNetherStar(501).setUnlocalizedName("Nether Star Block");
+        blocknetherstar = new BlockNetherStar(this.blocknetherstarblockID).setUnlocalizedName("Nether Star Block");
 		GameRegistry.registerBlock(blocknetherstar, "blocknetherstar");
 		LanguageRegistry.instance().addNameForObject(blocknetherstar, "en_US", "Nether Star Block");
         LanguageRegistry.instance().addNameForObject(blocknetherstar, "ru_RU", "Фу, не знать английский");
         
-        blockuranium = new BlockUranium(502).setUnlocalizedName("Block Uranium");
+        //blockuranium = new BlockUranium(502).setUnlocalizedName("Block Uranium");
+        blockuranium = new BlockUranium(this.blockuraniumblockID).setUnlocalizedName("Block Uranium");
 		GameRegistry.registerBlock(blockuranium, "blockuranium");
 		LanguageRegistry.instance().addNameForObject(blockuranium, "en_US", "Uranium Ore");
         LanguageRegistry.instance().addNameForObject(blockuranium, "ru_RU", "Фу, не знать английский");
         
-        furnaceuraniumidle = new FurnaceUranium(503, false).setUnlocalizedName("Uranium Furnace Idle").setCreativeTab(USM.USMTab);
-        furnaceuraniumactive = new FurnaceUranium(504, true).setUnlocalizedName("Uranium Furnace Active").setLightValue(0.8F);
+        //furnaceuraniumidle = new FurnaceUranium(503, false).setUnlocalizedName("Uranium Furnace Idle").setCreativeTab(USM.USMTab);
+        furnaceuraniumidle = new FurnaceUranium(this.furnaceuraniumidleblockID, false).setUnlocalizedName("Uranium Furnace Idle").setCreativeTab(USM.USMTab);
+        //furnaceuraniumactive = new FurnaceUranium(504, true).setUnlocalizedName("Uranium Furnace Active").setLightValue(0.8F);
+        furnaceuraniumactive = new FurnaceUranium(this.furnaceuraniumactiveblockID, true).setUnlocalizedName("Uranium Furnace Active").setLightValue(0.8F);
         GameRegistry.registerBlock(furnaceuraniumidle, "Uranium Furnace");
         GameRegistry.registerBlock(furnaceuraniumactive, "Uranium Furnace Active");
         GameRegistry.registerTileEntity(TileEntityFurnaceUranium.class, "FurnaceUranium");
@@ -106,23 +138,27 @@ public class USM {
         LanguageRegistry.instance().addNameForObject(furnaceuraniumidle, "en_US", "Uranium Furnace Idle");
         LanguageRegistry.instance().addNameForObject(furnaceuraniumidle, "Фу, не знать английский", "");
         
-        sworduranium = new SwordUranium(1551).setUnlocalizedName("Uranium Sword");
+        //sworduranium = new SwordUranium(1551).setUnlocalizedName("Uranium Sword");
+        sworduranium = new SwordUranium(this.sworduraniumitemID).setUnlocalizedName("Uranium Sword");
         GameRegistry.registerItem(sworduranium, "sworduranium");
         LanguageRegistry.instance().addNameForObject(sworduranium, "en_US", "Uranium Sword");
         LanguageRegistry.instance().addNameForObject(sworduranium, "Фу, не знать английский", "");
         
-        stickiron = new StickIron(1550).setUnlocalizedName("Iron Stick");
+        //stickiron = new StickIron(1550).setUnlocalizedName("Iron Stick");
+        stickiron = new StickIron(this.stickironitemID).setUnlocalizedName("Iron Stick");
         GameRegistry.registerItem(stickiron, "stickiron");
         LanguageRegistry.instance().addNameForObject(stickiron, "en_US", "Iron Stick");
         LanguageRegistry.instance().addNameForObject(stickiron, "Фу, не знать английский", "");
         
         
-        ingoturanium = new IngotUranium(1552).setUnlocalizedName("Uranium Ingot");
+        //ingoturanium = new IngotUranium(1552).setUnlocalizedName("Uranium Ingot");
+        ingoturanium = new IngotUranium(this.ingoturaniumitemID).setUnlocalizedName("Uranium Ingot");
         GameRegistry.registerItem(ingoturanium, "ingoturanium");
         LanguageRegistry.instance().addNameForObject(ingoturanium, "en_US", "Uranium Ingot");
         LanguageRegistry.instance().addNameForObject(ingoturanium, "Фу, не знать английский", "");
         
-        ingotinfuseduranium = new IngotInfusedUranium(1553).setUnlocalizedName("Infused Uranium Ingot");
+        //ingotinfuseduranium = new IngotInfusedUranium(1553).setUnlocalizedName("Infused Uranium Ingot");
+        ingotinfuseduranium = new IngotInfusedUranium(this.ingotinfuseduraniumitemID).setUnlocalizedName("Infused Uranium Ingot");
         GameRegistry.registerItem(ingotinfuseduranium, "ingotinfuseduranium");
         LanguageRegistry.instance().addNameForObject(ingotinfuseduranium, "en_US", "Infused Uranium Ingot");
         LanguageRegistry.instance().addNameForObject(ingotinfuseduranium, "Фу, не знать английский", "");
@@ -135,6 +171,7 @@ public class USM {
         OreDictionary.registerOre("oreUranium", new ItemStack(oreuranium));
         OreDictionary.registerOre("stickIron", new ItemStack(stickiron));
         OreDictionary.registerOre("ironStick", new ItemStack(stickiron));
+        OreDictionary.registerOre("ironRod", new ItemStack(stickiron));
         OreDictionary.registerOre("blockUranium", new ItemStack(blockuranium));
         
 //Крафты        
@@ -176,14 +213,4 @@ public class USM {
 		
 	}
 	public static final EnumToolMaterial Uranium = EnumHelper.addToolMaterial("Uranium", 3, 768, 9.0F, 71.0F, 50);
-//  @EventHandler    
-//	public void preLoad(FMLPreInitializationEvent event)
-//	{
-//	Configuration config = new Configuration(event.getSuggestedConfigurationFile());
-//	config.load();
-//	oreuraniumblockID = config.getBlock("uraniumore", 1000).getInt();
-//	config.save();
-//	}
-	
-	
 }

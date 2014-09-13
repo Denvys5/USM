7d7a15727877cd10b9c83ebfb9ff0a69b9bf9432
Denvys5 <denvys5@gmail.com>
2014-09-13 12:18:37 +0300

    0.2

--- assets/uraniumswordmod/lib/Config.java
+++ assets/uraniumswordmod/lib/Config.java
@@ -1,6 +1,7 @@
 package assets.uraniumswordmod.lib;
 
 import java.io.File;
+import java.nio.file.Path;
 
 import assets.uraniumswordmod.USM;
 import assets.uraniumswordmod.achievements.USMAchievementsList;
@@ -8,20 +9,20 @@ import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.Configuration;
+import net.minecraftforge.common.Property;
 import net.minecraftforge.oredict.OreDictionary;
 
 public class Config {
-
 	
 	public static Configuration config = new Configuration(new File(
 			"config/USM/UraniumSwordMod.cfg"));
 	
 	public static void ConfigMethod() {
 		config.load();
-		Config.ConfigBlockID();
-		Config.ConfigItemID();
-		Config.ConfigOreRegister();
-		Config.ConfigCrafts();
+		ConfigBlockID();
+		ConfigItemID();
+		ConfigTweakRegister();
+		ConfigCrafts();
 		config.save();
 	}
 
@@ -30,10 +31,13 @@ public class Config {
 	}
 
 
-	private static void ConfigOreRegister() {
-		OreRegistration.OreUraniumRegister = config.get("Tweaks", "Uranium Ore Ore Registration", 1).getInt();
-		OreRegistration.IngotUraniumRegister = config.get("Tweaks", "Uranium Ingot Ore Registration", 1).getInt();
-		OreRegistration.IronStickRegister = config.get("Tweaks", "Iron Stick Ore Registration", 1).getInt();
+	private static void ConfigTweakRegister() {
+		OreRegistration.OreUraniumRegister = config.get("Tweaks", "Uranium Ore Ore Registration", true).getBoolean(true);
+		OreRegistration.IngotUraniumRegister = config.get("Tweaks", "Uranium Ingot Ore Registration", true).getBoolean(true);
+		OreRegistration.IronStickRegister = config.get("Tweaks", "Iron Stick Ore Registration", true).getBoolean(true);
+		/*Property comment = config.get("Tweaks", "SomeConfigString", "nothing");
+		comment.comment = "This value can be read as a string!";*/
+		USM.UraniumSwordDamage = config.get("Tweaks", "Uranium Sword Base Damage", 1500.0).getDouble(1500.0);
 	}
 
 	private static void ConfigBlockID() {
